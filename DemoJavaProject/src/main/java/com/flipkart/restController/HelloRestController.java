b30117d1dea51381e39789293a72c3fe935cfe31
Pranay Bansal <44213415+PranayB200@users.noreply.github.com>
2021-06-16 15:38:39 +0530

    class diag

---
+++ DemoJavaProject/src/main/java/com/flipkart/restController/HelloRestController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.restController;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloRestController {
+	
+	 @GET
+	    public String getEmployees() {
+	        return "my dropwizard service";
+	    }
+
+}
